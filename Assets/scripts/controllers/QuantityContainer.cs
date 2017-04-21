f6c2b1ecb4eb15cf6827491c2583db724d30ebc2
Devan <devanpatel13@gmail.com>
2017-04-21 17:18:55 -0400

    ShoppingCart

---
+++ Assets/scripts/controllers/QuantityContainer.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class QuantityContainer : MonoBehaviour {
+
+	public Text quantityText;
+
+	void OnEnable()
+	{
+		EventManager.OnButtonClick += OnButtonClick;
+	}
+
+	void OnDisable()
+	{
+		EventManager.OnButtonClick -= OnButtonClick;
+	}
+
+	void OnButtonClick(SystemEnum.ButtonID buttonID)
+	{
+
+		if (buttonID !=  SystemEnum.ButtonID.UpArrow && buttonID != SystemEnum.ButtonID.DownArrow)
+		{
+			return;
+		}
+
+		Book book = SystemController.CurrentBook;
+
+		SystemEnum.BookType currentBookType = SystemController.CurrentBookTypeSelected;
+		if (buttonID == SystemEnum.ButtonID.UpArrow)
+		{
+
+
+			if (currentBookType == SystemEnum.BookType.New)
+			{
+				if (book.Quantity < book.NewStock)
+				{
+					book.Quantity++;
+				} else
+				{
+					book.Quantity = book.NewStock;
+				}
+			} else	if (currentBookType == SystemEnum.BookType.Used)
+			{
+				if (book.Quantity < book.UsedStock)
+				{
+					book.Quantity++;
+				} else
+				{
+					book.Quantity = book.UsedStock;
+				}
+			} else	if (currentBookType == SystemEnum.BookType.Rental)
+			{
+				if (book.Quantity < book.RentStock)
+				{
+					book.Quantity++;
+				} else
+				{
+					book.Quantity = book.RentStock;
+				}
+			} else if (currentBookType == SystemEnum.BookType.Ebook)
+			{
+				book.Quantity++;
+			}
+
+		}
+		if (buttonID == SystemEnum.ButtonID.DownArrow)
+		{
+
+			if (book.Quantity > 0)
+			{
+				book.Quantity--;
+			}
+		}
+		quantityText.text = book.Quantity + "";
+
+	}
+
+
+	public void ResetQuantity()
+	{
+		if (SystemController.CurrentBook == null) return;
+		SystemController.CurrentBook.Quantity = 0;
+		quantityText.text = SystemController.CurrentBook.Quantity + "";
+
+	}
+
+
+}
---
+++ Assets/scripts/handlers/PriceContainerHandler.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PriceContainerHandler : MonoBehaviour {
+
+	public Image NewButton, UsedButton, RentButton, EbookButton;
+	public QuantityContainer quantityContainer;
+	public Color selectedColor;
+
+	void OnEnable()
+	{
+		EventManager.OnButtonClick += OnButtonClick;
+	}
+
+	void OnDisable()
+	{
+		EventManager.OnButtonClick -= OnButtonClick;
+	}
+
+	void Update()
+	{
+		UpdateButtonColor();
+	}
+
+	void OnButtonClick(SystemEnum.ButtonID buttonID)
+	{
+
+		switch (buttonID)
+		{
+			case SystemEnum.ButtonID.NewButton:
+			{
+				SystemController.CurrentBookTypeSelected = SystemEnum.BookType.New;
+				quantityContainer.ResetQuantity();
+				break;
+			}
+			case SystemEnum.ButtonID.UsedButton:
+			{
+				SystemController.CurrentBookTypeSelected = SystemEnum.BookType.Used;
+				quantityContainer.ResetQuantity();
+
+				break;
+			}
+			case SystemEnum.ButtonID.RentButton:
+			{
+				SystemController.CurrentBookTypeSelected = SystemEnum.BookType.Rental;
+				quantityContainer.ResetQuantity();
+
+				break;
+			}
+			case SystemEnum.ButtonID.EbookButton:
+			{
+				SystemController.CurrentBookTypeSelected = SystemEnum.BookType.Ebook;
+				quantityContainer.ResetQuantity();
+				break;
+			}
+		}
+
+		UpdateButtonColor();
+
+	}
+
+	void UpdateButtonColor()
+	{
+		EbookButton.color = Color.white;
+		UsedButton.color = Color.white;
+		RentButton.color = Color.white;
+		NewButton.color = Color.white;;
+		switch (SystemController.CurrentBookTypeSelected)
+		{
+			case SystemEnum.BookType.New:
+			{
+				NewButton.color = selectedColor;
+				break;
+			}
+			case SystemEnum.BookType.Used:
+			{
+				UsedButton.color = selectedColor;
+				break;
+			}
+			case SystemEnum.BookType.Rental:
+			{
+				RentButton.color = selectedColor;
+				break;
+			}
+			case SystemEnum.BookType.Ebook:
+			{
+				EbookButton.color = selectedColor;
+				break;
+			}
+
+		}
+	}
+}
